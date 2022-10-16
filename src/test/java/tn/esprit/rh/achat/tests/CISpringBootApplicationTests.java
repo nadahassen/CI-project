0fe333fb5047a0fd35951ac56bd00855ee091219
Hiba Farhat <hiba.farhat@esprit.tn>
2022-10-16 21:47:20 +0200

    add test package

---
+++ src/test/java/tn/esprit/rh/achat/tests/CISpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.rh.achat.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CISpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
