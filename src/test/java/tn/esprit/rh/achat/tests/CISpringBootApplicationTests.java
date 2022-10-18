2dee0de2e4422c156a1ef7aa971b7d57120ad7b5
BenkraiemSiwar <siwar.benkraiem@esprit.tn>
2022-10-18 09:47:20 +0100

    update

--- src/test/java/tn/esprit/rh/achat/tests/CISpringBootApplicationTests.java
+++ src/test/java/tn/esprit/rh/achat/tests/CISpringBootApplicationTests.java
@@ -3,7 +3,7 @@ package tn.esprit.rh.achat.tests;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CISpringBootApplicationTests {
 
 	/*@Test
