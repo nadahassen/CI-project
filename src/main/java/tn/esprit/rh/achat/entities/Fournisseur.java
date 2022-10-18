8e113ab8998673b39cfca1feb848264ac1a02878
BenkraiemSiwar <siwar.benkraiem@esprit.tn>
2022-10-18 10:17:25 +0100

    update

--- src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
+++ src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
@@ -15,7 +15,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
