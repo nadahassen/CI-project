ff34ba14599b12669e0feda6a998aecffc0a990c
Rihab <rihab.haddad@esprit.tn>
2022-10-23 19:38:13 +0100

    Add Dockerfile

--- src/test/java/tn/esprit/rh/achat/Tests/SecteurActiviteServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/Tests/SecteurActiviteServiceImplTest.java
@@ -2,29 +2,17 @@
 package tn.esprit.rh.achat.Tests;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
-import java.text.ParseException;
-import java.util.List;
-
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import tn.esprit.rh.achat.entities.SecteurActivite;
-import tn.esprit.rh.achat.repositories.SecteurActiviteRepository;
-import tn.esprit.rh.achat.services.ISecteurActiviteService;
 
 
 @TestMethodOrder(OrderAnnotation.class)
 @SpringBootTest
 public class SecteurActiviteServiceImplTest {
-	@Autowired
-	ISecteurActiviteService secteurService;
+	//@Autowired
+	//ISecteurActiviteService secteurService;
 	
 
 	//@Test
